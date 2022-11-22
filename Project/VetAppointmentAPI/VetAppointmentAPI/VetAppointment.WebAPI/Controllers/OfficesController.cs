297d7f5d7fccb720cbac40b62d40de6e1f77378e
Puscasu Bogdan <bogdan_puscasu2001@yahoo.com>
2022-11-22 15:54:25 +0200

    user&office tests and api

---
+++ Project/VetAppointmentAPI/VetAppointmentAPI/VetAppointment.WebAPI/Controllers/OfficesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VetAppointment.Application.Repositories.Interfaces;
+using VetAppointment.Domain.Entities;
+
+namespace VetAppointment.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfficesController : ControllerBase
+    {
+        private readonly IOfficeRepository officeRepository;
+
+        public OfficesController(IOfficeRepository officeRepository)
+        {
+            this.officeRepository = officeRepository;
+        }
+
+        // GET: api/<OfficesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(officeRepository.All());
+        }
+
+        // GET api/<OfficesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            return Ok(officeRepository.Get(id));
+        }
+
+        // POST api/<OfficesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] string address)
+        {
+            Office office = new Office(address);
+            officeRepository.Add(office);
+            officeRepository.SaveChanges();
+
+            return Created(nameof(Office), office);
+        }
+
+        // PUT api/<OfficesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OfficesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            Office office = officeRepository.Get(id);
+            officeRepository.Delete(office);
+            officeRepository.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Project/VetAppointmentAPI/VetAppointmentAPI/VetAppointment.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VetAppointment.Application.Repositories.Interfaces;
+using VetAppointment.Domain.Entities;
+
+namespace VetAppointment.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(userRepository.All());
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            return Ok(userRepository.Get(id));
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public IActionResult Post([FromBody] string name, [FromBody] string password, [FromBody] bool hasOffice)
+        {
+            User user = new User(name, password, hasOffice);
+            userRepository.Add(user);
+            userRepository.SaveChanges();
+
+            return Created(nameof(User), user);
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            User user = userRepository.Get(id);
+            userRepository.Delete(user);
+            userRepository.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
