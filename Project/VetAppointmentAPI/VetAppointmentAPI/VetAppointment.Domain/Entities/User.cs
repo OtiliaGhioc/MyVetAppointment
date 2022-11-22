a47ca70524d89ab5a46c942346862631d2605698
Axinte Calin-Cristian <calinaxinte01@gmail.com>
2022-11-22 16:15:56 +0200

    Create Appointments and MedicalEntry Controllers
    - All major operations covered for the models.
    - Data validation for object creation / modification / deletion.
    - Serialization inside the response bodies.
    - DTOs can be used to modify object data that can be further updated.
    - Custom DTOs for the different operations.

--- Project/VetAppointmentAPI/VetAppointmentAPI/VetAppointment.Domain/Entities/User.cs
+++ Project/VetAppointmentAPI/VetAppointmentAPI/VetAppointment.Domain/Entities/User.cs
@@ -1,20 +1,42 @@
-﻿namespace VetAppointment.Domain.Entities
+﻿using VetAppointment.Domain.Helpers;
+
+namespace VetAppointment.Domain.Entities
 {
     public class User
     {
-        public User(string username, string password, bool hasOffice)
+        public User(string username, string password)
         {
             UserId = Guid.NewGuid();
             Username = username;
             Password = CalculatePasswordHash(password);
-            HasOffice = hasOffice;
         }
 
         public Guid UserId { get; private set; }
         public string Username { get; private set; }
         public string Password { get; private set; }
-        public bool HasOffice { get; private set; }
+        public bool HasOffice { get; private set; } = false;
+        public Guid OfficeId { get; private set; }
         public Office? UserOffice { get; set; }
+
+        public Result RegisterOfficeToUser(Office office)
+        {
+            if (office == null)
+                return Result.Failure("Cannot register null object as office!");
+            UserOffice = office;
+            OfficeId = office.OfficeId;
+            HasOffice = true;
+            return Result.Success();
+        }
+
+        public Result UnregisterOfficeFromUser()
+        {
+            if (UserOffice == null)
+                return Result.Failure("User does not have an office registered!");
+            UserOffice = null;
+            OfficeId = Guid.Empty;
+            HasOffice = false;
+            return Result.Success();
+        }
 
         private string CalculatePasswordHash(string password)
         {
---
+++ Project/VetAppointmentAPI/VetAppointmentAPI/VetAppointment.WebAPI/Controllers/AppointmentsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VetAppointment.Application.Repositories.Interfaces;
+using VetAppointment.Domain.Entities;
+using VetAppointment.WebAPI.Dtos.AppointmentDtos;
+
+namespace VetAppointment.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IAppointmentRepository appointmentRepository;
+        private readonly IUserRepository userRepository;
+        public AppointmentsController(IAppointmentRepository appointmentRepository, IUserRepository userRepository)
+        {
+            this.appointmentRepository = appointmentRepository;
+            this.userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(appointmentRepository.All().ToList().Select(item => new AppointmentDetailDto(item)));
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] AppointmentCreateDto appointmentDto)
+        {
+            User? appointee = userRepository.Get(appointmentDto.AppointeeId);
+            if (appointee == null)
+                return NotFound();
+
+            User? appointer = userRepository.Get(appointmentDto.AppointerId);
+            if (appointer == null)
+                return NotFound();
+
+
+            Appointment appointment = new Appointment(appointer, appointee, DateTime.Now, appointmentDto.Description, appointmentDto.Type);
+            appointmentRepository.Add(appointment);
+            appointmentRepository.SaveChanges();
+
+            AppointmentDetailDto appointmentDetail = new AppointmentDetailDto(appointment);
+            return Created(nameof(Get), appointmentDetail);
+        }
+
+        [HttpPut("{appointmentId:guid}")]
+        public IActionResult Update(Guid appointmentId, [FromBody] AppointmentModifyDto appointmentDto)
+        {
+            Appointment? appointment = appointmentRepository.Get(appointmentId);
+            if (appointment == null)
+                return NotFound();
+            appointment = appointmentDto.ApplyModificationsToModel(appointment);
+            appointmentRepository.Update(appointment);
+            appointmentRepository.SaveChanges();
+            return Ok(new AppointmentDetailDto(appointment));
+        }
+
+        [HttpDelete("{appointmentId:guid}")]
+        public IActionResult Delete(Guid appointmentId)
+        {
+            Appointment? appointment = appointmentRepository.Get(appointmentId);
+            if (appointment == null)
+                return NotFound();
+            appointmentRepository.Delete(appointment);
+            appointmentRepository.SaveChanges();
+            return NoContent();
+        }
+    }
+}
